c651fca567cd8bb3e38ba226913fe3c080dcc0a9
Pamela <pamablang@gmail.com>
2019-08-03 18:12:20 +1000

    Add edit event function

--- routes/events.rb
+++ routes/events.rb
@@ -7,6 +7,11 @@ get "/events/:id" do
     @event = Event.find(params[:id])
     erb :event_details
 end
+
+get "/events/:id/edit" do
+    @event = Event.find(params[:id])
+    erb :edit_event
+end
    
 get '/events/new/form' do
     redirect '/login' unless session[:user_id]
@@ -20,7 +25,20 @@ post '/events/new' do
     event.date_time = "#{params[:date]} #{params[:time]}"
     event.location = params[:location]
     event.description = params[:description]
-    event.creator_id = current_user.id
+    event.user_id = current_user.id
     event.save
     redirect '/events'
+end
+
+put "/events/:id" do
+    redirect '/login' unless current_user
+    # binding.pry
+    event = Event.find(params[:id])
+    event.name = params[:name]
+    event.date_time = "#{params[:date]} #{params[:time]}"
+    event.location = params[:location]
+    event.description = params[:description]
+    event.user_id = current_user.id
+    event.save
+    redirect "/events/#{params[:id]}"
 end
