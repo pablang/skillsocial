1fdd5f83d6acb0a1a2de5a376bcb64735fbcfa7f
Pamela <pamablang@gmail.com>
2019-08-03 16:25:10 +1000

    Add a rollcall on profile dashboard

--- routes/subscribed_users.rb
+++ routes/subscribed_users.rb
@@ -1,8 +1,7 @@
 post "/subscribed_users/new" do
-
-user = User.new
-user.user_id = params[:user_id]
-user.event_id = params[:event_id]
-user.save
+sub_user = EventSubscription.new
+sub_user.subscribed_user_id = params[:user_id]
+sub_user.subscribed_event_id = params[:event_id]
+sub_user.save
 redirect "/events/#{params[:event_id]}"
 end
